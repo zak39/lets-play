e295038fccfb757428423ed11ebff64797119b34
zak39 <fotia.baptiste@hotmail.com>
2019-10-01 10:48:38 +0200

    Init exercice with dotnet for Linux

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lets_play
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
